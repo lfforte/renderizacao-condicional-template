c4b4b445d971d9febf8f5637e1527a7b6aa09b52
Luiz Fernando Forte <LFFORTE@GMAIL.COM>
2024-03-15 20:04:36 -0300

    Exercícios aula renderização condicionais

--- src/components/TelaLogin/TelaLogin.js
+++ src/components/TelaLogin/TelaLogin.js
@@ -1,13 +1,23 @@
 import { Form, FormContainer, Input, SendButton, StyledLabel, RegisterButton } from "./styled";
 
-function TelaLogin() {
+function TelaLogin(props) {
+  let loginUser = ''
+  let passUser = ''
+
   const login = () => {
     // validação de login ainda será visto mais pra frente no curso
-    alert("Login bem sucedido, mas falta fazer a prática 3 e remover esse alerta")
+    /* alert("Login bem sucedido, mas falta fazer a prática 3 e remover esse alerta") */
+    if (loginUser === '' && loginUser === '') {
+      { props.setTela('Principal') }
+    } else {
+      alert("Erro ao fazer o login !")
+    }
+
   }
 
   const mostrarTelaCadastro = () => {
-    alert("Faça a prática 2 e remova esse alerta")
+    /* alert("Faça a prática 2 e remova esse alerta") */
+    { props.setTela('Cadastro') }
   }
 
   return (
